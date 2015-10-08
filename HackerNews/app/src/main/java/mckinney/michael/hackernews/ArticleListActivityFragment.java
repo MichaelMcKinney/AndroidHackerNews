064b60e1e1891b43ab92a5b1cbd8f88b93ce2bbb
Michael McKinney <mmckinney277@gmail.com>
2015-10-08 09:28:23 -0400

    first commit

---
+++ HackerNews/app/src/main/java/mckinney/michael/hackernews/ArticleListActivityFragment.java
@@ -0,0 +1,22 @@
+package mckinney.michael.hackernews;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArticleListActivityFragment extends Fragment {
+
+    public ArticleListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_article_list, container, false);
+    }
+}
